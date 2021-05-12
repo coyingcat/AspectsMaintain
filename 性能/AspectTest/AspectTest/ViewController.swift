cee7db5e4b93a35feba5b96f3b48fac154deb2e9
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-12 13:41:50 +0800

    test aspect

--- 性能/AspectTest/AspectTest/ViewController.swift
+++ 性能/AspectTest/AspectTest/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
+
+import Aspect
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+
+        _ = try? hook(selector: #selector(ViewController.test(id:name:)), strategy: .before) { (_, id: Int, name: String) in
+            print("ha ha ha")
+        }
+
+
+        
     }
+    
+    
 
-
+    @objc dynamic func test(id: Int, name: String) {
+        print("come on")
+    }
+    
+    
+    
+    
+    @IBAction func btnClick(_ sender: Any) {
+        test(id: 666, name: "click")
+    }
+    
+    
 }
 
