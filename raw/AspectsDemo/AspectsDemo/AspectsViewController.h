3ecc059f322a70b9635e74ac41d01644d77fe29c
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-18 11:03:33 +0800

    // 一个 Method,
    // 通过 sel 、 name 、 imp 确定

--- raw/AspectsDemo/AspectsDemo/AspectsViewController.h
+++ raw/AspectsDemo/AspectsDemo/AspectsViewController.h
@@ -10,6 +10,38 @@
 
 @interface AspectsViewController : UIViewController
 
+
+
+// 该实例方法，的底层是
+
+// Method_t
+
+// 这样一个结构体
+
+
+// 存放 sel ( 即， 方法编号  buttonPressed:)
+
+// sel 不可，唯一标识一个方法
+
+// 所以，给他一个，签名信息
+
+
+
+
+
+// 一个 Method,
+
+// 通过 sel 、 name 、 imp 确定
+
+
+
 - (IBAction)buttonPressed:(id)sender;
 
 @end
+
+
+
+
+
+
+
