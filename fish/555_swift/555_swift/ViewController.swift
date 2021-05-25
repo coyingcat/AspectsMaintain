aa5c684a85611ee95775718d235f830b57e30850
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-25 09:41:06 +0800

    fish hook  swift

---
+++ fish/555_swift/555_swift/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  555_swift
+//
+//  Created by Jz D on 2021/5/25.
+//
+
+import UIKit
+
+
+typealias NewPrintf = @convention(thin) (String, Any...) -> Void
+
+func newPrinf(str: String, arg: Any...) -> Void {
+    string = "test success"
+}
+
+public func fishhookPrint(newMethod: UnsafeMutableRawPointer) {
+    var oldMethod: UnsafeMutableRawPointer?
+    rebindSymbol("printf", replacement: newMethod, replaced: &oldMethod)
+}
+
+var string = ""
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        fishhookPrint(newMethod: unsafeBitCast(newPrinf as NewPrintf, to: UnsafeMutableRawPointer.self))
+
+        Test.print(withStr: "Hello World")
+    }
+
+
+}
+
