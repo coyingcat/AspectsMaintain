b6576a8e4e5c70a9ec6e349c810d6077676162d2
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-31 16:14:24 +0800

    @convention(thin)

--- fish/555_swift/555_swift/ViewController.swift
+++ fish/555_swift/555_swift/ViewController.swift
@@ -24,14 +24,15 @@ var string = ""
 class ViewController: UIViewController {
 
     
-    var oldMethod: UnsafeMutableRawPointer?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        var oldMethod: UnsafeMutableRawPointer?
         rebindSymbol("printf", replacement: unsafeBitCast(newPrinf as NewPrintf, to: UnsafeMutableRawPointer.self), replaced: &oldMethod)
         Test.print(withStr: "Hello World \n\n\n")
+        print(oldMethod == nil)
     }
 
 
